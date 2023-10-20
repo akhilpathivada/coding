4c6486ef8a863f1804d7e16984303aad950416c9
AkhilPathivada <iakhilp@amazon.com>
2023-10-20 18:38:17 +0530

    added@ Fri Oct 20 18:38:17 IST 2023

---
+++ interview/src/tree/HouseRobberIII.java
@@ -0,0 +1,45 @@
+/**
+ * Date 09/04/2022
+ *
+ * @author akhilpathivada
+ *
+ * https://leetcode.com/problems/house-robber-iii/
+ *
+ * Time Complexity : O(N)
+ * Space Complexity : O(N)
+ */
+package tree;
+
+
+public class HouseRobberIII {
+
+    private int[] dfs(TreeNode root) {
+        // base case
+        if (root == null) {
+            return new int[2];
+        }
+        int[] dp_left = dfs(root.left);
+        int[] dp_right = dfs(root.right);
+        int[] result = new int[2];
+        // if we dont want to include root
+        result[0] = Math.max(dp_left[0], dp_left[1]) + Math.max(dp_right[0], dp_right[1]);
+        // if we want to include root
+        result[1] = root.data + dp_left[0] + dp_right[0];
+        return result;
+    }
+
+    private int rob(TreeNode root) {
+        int[] result = dfs(root);
+        return Math.max(result[0], result[1]);
+    }
+
+    public static void main(String[] args) {
+        TreeNode root = new TreeNode(3);
+        root.left = new TreeNode(2);
+        root.left.right = new TreeNode(3);
+        root.right = new TreeNode(3);
+        root.right.right = new TreeNode(1);
+
+        System.out.println(new HouseRobberIII().rob(root));
+    }
+}
