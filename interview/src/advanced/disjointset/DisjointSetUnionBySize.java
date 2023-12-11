818771fd696003a15d053778a242d6172a95f6af
AkhilPathivada <iakhilp@amazon.com>
2023-12-11 08:56:25 +0530

    added@ Mon Dec 11 08:56:25 IST 2023

--- interview/src/advanced/disjointset/DisjointSetUnionBySize.java
+++ interview/src/advanced/disjointset/DisjointSetUnionBySize.java
@@ -6,4 +6,61 @@
 package advanced.disjointset;
 
 public class DisjointSetUnionBySize {
+
+    int[] size; // stores size
+    int[] parent; // stores parent of each node
+
+    public DisjointSetUnionBySize(int n) {
+        // create with n + 1 size: so that it would be useful for 0-indexed and 1-indexed as well
+        size = new int[n + 1];
+        parent = new int[n + 1];
+
+        // initially every node itself is a disjoint set
+        // so parent of a node is itself and also
+        // the size of every node is 1
+        for (int i = 0; i <= n; ++i) {
+            parent[i] = i;
+            size[i] = 1;
+        }
+    }
+
+    // to find the ultimate parent of a node
+    // and do "path compression"
+    public int findUltimateParent(int node) {
+        // this the ultimate parent (root)
+        if (parent[node] == node) {
+            return node;
+        }
+        parent[node] = findUltimateParent(parent[node]);
+        return parent[node];
+    }
+
+    public void unionBySize(int u, int v) {
+        int ultimateParentOfU = findUltimateParent(u);
+        int ultimateParentOfV = findUltimateParent(v);
+        // if both are already in same set
+        if (ultimateParentOfU == ultimateParentOfV) {
+            return;
+        }
+        if (size[ultimateParentOfU] < size[ultimateParentOfV]) {
+            parent[ultimateParentOfU] = ultimateParentOfV;
+            size[ultimateParentOfV] += size[ultimateParentOfU];
+        } else {
+            parent[ultimateParentOfV] = ultimateParentOfU;
+            size[ultimateParentOfU] += size[ultimateParentOfV];
+        }
+    }
+
+    public static void main(String[] args) {
+        int n = 7;
+        DisjointSetUnionBySize disjointSet = new DisjointSetUnionBySize(n);
+        disjointSet.unionBySize(1, 2);
+        disjointSet.unionBySize(2, 3);
+        disjointSet.unionBySize(4, 5);
+        disjointSet.unionBySize(6, 7);
+        disjointSet.unionBySize(5, 6);
+        System.out.println(disjointSet.findUltimateParent(3) == disjointSet.findUltimateParent(7));
+        disjointSet.unionBySize(3, 7);
+        System.out.println(disjointSet.findUltimateParent(3) == disjointSet.findUltimateParent(7));
+    }
 }
