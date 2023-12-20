fb8578c4b0642d1afa42f06a0bc04cc307eb7bf6
AkhilPathivada <iakhilp@amazon.com>
2023-12-20 12:48:37 +0530

    learnt@ Wed Dec 20 12:48:37 IST 2023

---
+++ interview/src/patternmatch/KMPAlgorithm.java
@@ -0,0 +1,68 @@
+/**
+ *
+ * https://www.geeksforgeeks.org/kmp-algorithm-for-pattern-searching/
+ *
+ * Time Complexity: O(N + M) where N is the length of the text and M is the length of the pattern to be found.
+ * Space Complexity: O(M)
+ * */
+package patternmatch;
+
+public class KMPAlgorithm {
+
+    // lps :: Longest Prefix Suffix
+    // compute lps array to maintain size of suffix which is same as prefix, where
+    // lps[i] -> size of suffix ending at 'i' which has a matching prefix
+    private int[] computeLPS(char[] pattern, int m) {
+        int[] lps = new int[m];
+        int len = 0; // length of the previous longest prefix suffix
+        lps[0] = 0; // lps[0] is always 0
+        // the loop calculates lps[i] for i = 1 to m - 1
+        for (int i = 1; i < m; ) {
+            // characters match : found a suffix (ending here at 'i')
+            // which has same prefix with size of 'len + 1'
+            if (pattern[i] == pattern[len]) {
+                lps[i] = len + 1;
+                ++len;
+                ++i;
+            } else {
+                if (len != 0) { // 'len' should go back to the point where, the next highest string match
+                    len = lps[len - 1];
+                } else { // no matching prefix found, so mark lps[i] = 0
+                    lps[i] = 0;
+                    ++i;
+                }
+            }
+        }
+        return lps;
+    }
+
+    private boolean KMP(char[] text, char[] pattern) {
+        int m = pattern.length;
+        int n = text.length;
+        int[] lps = computeLPS(pattern, m);
+        int i = 0;
+        int j = 0;
+        while (i < n && j < m) {
+            if (text[i] == pattern[j]) {
+                ++i;
+                ++j;
+            } else {
+                if (j != 0) {
+                    j = lps[j - 1];
+                } else {
+                    ++i;
+                }
+            }
+            if (j == m) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public static void main(String[] args) {
+        String text = "ABABDABACDABABCABAB";
+        String pattern = "ABABCABAB";
+        System.out.println(new KMPAlgorithm().KMP(text.toCharArray(), pattern.toCharArray()));
+    }
+}
---
+++ interview/src/patternmatch/LongestHappyPrefix.java
@@ -0,0 +1,41 @@
+/**
+ * https://leetcode.com/problems/longest-happy-prefix/description/
+ * https://www.codingninjas.com/studio/problems/longest-prefix-which-is-suffix_3146849
+ *
+ * Time Complexity: O(N)
+ * Space Complexity: O(N)
+ * */
+package patternmatch;
+
+import java.util.Arrays;
+
+public class LongestHappyPrefix {
+
+    public String longestPrefix(String s) {
+        int n = s.length();
+        // logic of computing the lps[] from KMP Algorithm
+        int[] lps = new int[n];
+        int len = 0;
+        lps[0] = 0;
+        for (int i = 1; i < n; ) {
+            if (s.charAt(i) == s.charAt(len)) {
+                lps[i] = len + 1;
+                ++len;
+                ++i;
+            } else {
+                if (len != 0) {
+                    len = lps[len - 1];
+                } else {
+                    lps[i] = 0;
+                    ++i;
+                }
+            }
+        }
+        return s.substring(0, lps[n - 1]);
+    }
+
+    public static void main(String[] args) {
+        String s = "ababab";
+        System.out.println(new LongestHappyPrefix().longestPrefix(s));
+    }
+}
