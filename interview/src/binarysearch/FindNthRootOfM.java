7926f240b79f8f8e4a247db7327aafaa3e05f49d
AkhilPathivada <iakhilp@amazon.com>
2023-12-25 16:28:22 +0530

    refactor@ Mon Dec 25 16:28:22 IST 2023

--- interview/src/binarysearch/FindNthRootOfM.java
+++ interview/src/binarysearch/FindNthRootOfM.java
@@ -6,9 +6,31 @@
  *
  * https://www.codingninjas.com/studio/problems/nth-root-of-m_1062679
  *
- *
+ * Time Complexity: O(log(m) * log(n))
  */
 package binarysearch;
 
 public class FindNthRootOfM {
+
+    private int NthRoot(int n, int m) {
+        int low = 1;
+        int high = m;
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+            int value = (int) Math.pow(mid, n);
+            if (value == m) {
+                return mid;
+            } else if (value < m) {
+                low = mid + 1;
+            } else {
+                high = mid - 1;
+            }
+        }
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        int n = 3, m = 27;
+        System.out.println(new FindNthRootOfM().NthRoot(n, m));
+    }
 }
