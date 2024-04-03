d46d31a74a625cb5548d5d40a77e0c9152c4aa0a
AkhilPathivada <iakhilp@amazon.com>
2024-04-03 09:41:36 +0530

    learnt@ Wed Apr  3 09:41:36 IST 2024

---
+++ interview/src/design/InsertDeleteGetRandom.java
@@ -0,0 +1,79 @@
+/**
+ * @author akhilpathivada
+ * <p>
+ * date : 03/04/24
+ * time: 09:13
+ *
+ * https://leetcode.com/problems/insert-delete-getrandom-o1/description/
+ *
+ */
+package design;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Random;
+
+public class InsertDeleteGetRandom {
+
+    private static class RandomizedSet {
+
+        private final List<Integer> list;
+
+        private final Map<Integer, Integer> hashMap;
+
+        private final Random random;
+
+        public RandomizedSet() {
+            list = new ArrayList<>();
+            hashMap = new HashMap<>();
+            random = new Random();
+        }
+
+        public boolean insert(int val) {
+            if (!hashMap.containsKey(val)) {
+                // store val in the list: by default added at the end
+                list.add(val);
+                // store (val, index) in the map:
+                // where, index is the position of val in the list
+                hashMap.put(val, list.size() - 1);
+                return true;
+            }
+            return false;
+        }
+
+        public boolean remove(int val) {
+            if (hashMap.containsKey(val)) {
+                int indexOfValInTheList = hashMap.get(val);
+                int lastIndexOfTheList = list.size() - 1;
+                int lastElementOfTheList = list.get(lastIndexOfTheList);
+                // replace the 'val' with last element of the list
+                list.set(indexOfValInTheList, lastElementOfTheList);
+                // update the index of last element in the map
+                hashMap.put(lastElementOfTheList, indexOfValInTheList);
+                // remove the last element from list
+                list.remove(lastIndexOfTheList);
+                // remove the val from hash map
+                hashMap.remove(val);
+                return true;
+            }
+            return false;
+        }
+
+        public int getRandom() {
+            return list.get(random.nextInt(list.size()));
+        }
+    }
+
+    public static void main(String[] args) {
+        RandomizedSet randomizedSet = new RandomizedSet();
+        System.out.println(randomizedSet.insert(1)); // Inserts 1 to the set. Returns true as 1 was inserted successfully.
+        System.out.println(randomizedSet.remove(2)); // Returns false as 2 does not exist in the set.
+        System.out.println(randomizedSet.insert(2)); // Inserts 2 to the set, returns true. Set now contains [1,2].
+        System.out.println(randomizedSet.getRandom()); // getRandom() should return either 1 or 2 randomly.
+        System.out.println(randomizedSet.remove(1)); // Removes 1 from the set, returns true. Set now contains [2].
+        System.out.println(randomizedSet.insert(2)); // 2 was already in the set, so return false.
+        System.out.println(randomizedSet.getRandom()); // Since 2 is the only number in the set, getRandom() will always return 2.
+    }
+}
--- interview/src/graph/dijkstra/MinimumMultiplicationsToReachEnd.java
+++ interview/src/graph/dijkstra/MinimumMultiplicationsToReachEnd.java
@@ -1,9 +1,69 @@
 /**
  * @author akhilpathivada
- *
+ * <p>
  * date : 03/04/24
  * time: 07:55
  *
+ * https://www.geeksforgeeks.org/problems/minimum-multiplications-to-reach-end/1
+ *
+ * Time Complexity: O(100000 * N)
+ * Space Complexity: O(100000 * N)
  */
-package graph.dijkstra;public class MinimumMultiplicationsToReachEnd {
+package graph.dijkstra;
+
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class MinimumMultiplicationsToReachEnd {
+
+    private static final int MOD = 100000;
+
+    private static class Pair {
+
+        private final int vertex;
+
+        private final int distance;
+
+        private Pair(int vertex, int distance) {
+            this.vertex = vertex;
+            this.distance = distance;
+        }
+    }
+
+    private int minimumMultiplications(int[] arr, int start, int end) {
+        // multiplications required to reach the node i.e., distance to reach the node
+        int[] dist = new int[MOD];
+        // fill the distance with infinity
+        Arrays.fill(dist, Integer.MAX_VALUE);
+        dist[start] = 0;
+        Queue<Pair> queue = new LinkedList<>();
+        queue.add(new Pair(start, dist[start]));
+        while (!queue.isEmpty()) {
+            int currentNode = queue.peek().vertex;
+            int currentDistance = queue.peek().distance;
+            queue.remove();
+            for (int num : arr) {
+                int newNode = (currentNode * num) % MOD;
+                if (currentDistance + 1 < dist[newNode]) {
+                    dist[newNode] = currentDistance + 1;
+                    if (newNode == end) {
+                        return dist[newNode];
+                    }
+                    queue.add(new Pair(newNode, dist[newNode]));
+                }
+            }
+        }
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        // input-1
+        int[] arr = { 2, 5, 7 };
+        int start = 3, end = 30;
+//        // input-2
+//        int[] arr = { 3, 4, 65 };
+//        int start = 7, end = 66175;
+        System.out.print(new MinimumMultiplicationsToReachEnd().minimumMultiplications(arr, start, end));
+    }
 }
