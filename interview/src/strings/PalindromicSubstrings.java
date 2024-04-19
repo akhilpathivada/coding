8347b3c78f59d469831c9687a79f6da38815c161
AkhilPathivada <iakhilp@amazon.com>
2024-04-19 11:37:16 +0530

    refactor@ Fri Apr 19 11:37:16 IST 2024

--- interview/src/strings/PalindromicSubstrings.java
+++ interview/src/strings/PalindromicSubstrings.java
@@ -8,12 +8,18 @@ package strings;
 
 public class PalindromicSubstrings {
 
-    int count = 0;
+    private int count = 0;
+
+    private void extendPalindrome(final String s, int left, int right) {
+        while (left >= 0 && right < s.length() && s.charAt(left--) == s.charAt(right++)) {
+            ++count;
+        }
+    }
+
     private int countSubstrings(String s) {
         if (s == null || s.isEmpty()) {
             return 0;
         }
-
         for (int i = 0; i < s.length(); ++i) { // assume 'i' as the midpoint
             // odd length
             extendPalindrome(s, i, i);
@@ -21,14 +27,6 @@ public class PalindromicSubstrings {
             extendPalindrome(s, i, i + 1);
         }
         return count;
-    }
-
-    private void extendPalindrome(String s, int left, int right) {
-        while (left >= 0 && right < s.length() && s.charAt(left) == s.charAt(right)) {
-            --left;
-            ++right;
-            ++count;
-        }
     }
 
     public static void main(String[] args) {
