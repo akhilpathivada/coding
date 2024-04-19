190a47317a4ba88888dd03aaddf51a14abf4062a
AkhilPathivada <iakhilp@amazon.com>
2024-04-19 11:14:50 +0530

    refactor@ Fri Apr 19 11:14:50 IST 2024

--- interview/src/greedy/KClosestPointsToOrigin.java
+++ interview/src/greedy/KClosestPointsToOrigin.java
@@ -10,13 +10,28 @@
  */
 package greedy;
 
-import java.util.Arrays;
-import java.util.Comparator;
+import java.util.*;
 
 public class KClosestPointsToOrigin {
-        private int[][] kClosest(int[][] points, int k) {
+        private int[][] kClosest2(int[][] points, int k) {
                 Arrays.sort(points, Comparator.comparing(p -> p[0] * p[0] + p[1] * p[1]));
                 return Arrays.copyOfRange(points, 0, k);
+        }
+
+        private int[][] kClosest(int[][] points, int k) {
+                PriorityQueue<int[]> maxHeap = new PriorityQueue<int[]>((p1, p2)
+                        -> p2[0] * p2[0] + p2[1] * p2[1] - p1[0] * p1[0] - p1[1] * p1[1]);
+                for (int[] p : points) {
+                        maxHeap.offer(p);
+                        if (maxHeap.size() > k) {
+                                maxHeap.poll();
+                        }
+                }
+                int[][] result = new int[k][2];
+                while (k-- > 0) {
+                        result[k] = maxHeap.poll();
+                }
+                return result;
         }
 
         public static void main(String[] args) {
