093823af050317a85a8103eecd2ebba310a0c83f
AkhilPathivada <iakhilp@amazon.com>
2024-04-19 11:16:24 +0530

    refactor@ Fri Apr 19 11:16:24 IST 2024

---
+++ interview/src/heap/KClosestPointsToOrigin.java
@@ -0,0 +1,38 @@
+/**
+ * Date 11/05/2022
+ *
+ * @author akhilpathivada
+ *
+ * https://leetcode.com/problems/k-closest-points-to-origin/
+ *
+ * Time Complexity : O(N * log(N)) -> for sorting
+ * Space Complexity : O(N) -> space needed for sorting
+ */
+package heap;
+
+import java.util.*;
+
+public class KClosestPointsToOrigin {
+
+    private int[][] kClosest(int[][] points, int k) {
+        PriorityQueue<int[]> maxHeap = new PriorityQueue<int[]>((p1, p2)
+                -> p2[0] * p2[0] + p2[1] * p2[1] - p1[0] * p1[0] - p1[1] * p1[1]);
+        for (int[] p : points) {
+            maxHeap.offer(p);
+            if (maxHeap.size() > k) {
+                maxHeap.poll();
+            }
+        }
+        int[][] result = new int[k][2];
+        while (k-- > 0) {
+            result[k] = maxHeap.poll();
+        }
+        return result;
+    }
+
+    public static void main(String[] args) {
+        int[][] points = { { 1, 3 }, { -2, 2 } };
+        int k = 1;
+        System.out.println(Arrays.deepToString(new KClosestPointsToOrigin().kClosest(points, k)));
+    }
+}
