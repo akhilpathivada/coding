70f6709ff22c14576759c60fcb937b9e15931c7b
AkhilPathivada <iakhilp@amazon.com>
2024-04-19 17:17:11 +0530

    refactor@ Fri Apr 19 17:17:11 IST 2024

---
+++ interview/src/slidingwindow/MaximizeTheConfusionOfAnExam.java
@@ -0,0 +1,37 @@
+package slidingwindow;
+
+/**
+ * Date 19/04/24
+ * Time 16:40
+ *
+ * @author akhilpathivada
+ *
+ * https://leetcode.com/problems/maximize-the-confusion-of-an-exam/description/
+ *
+ */
+public class MaximizeTheConfusionOfAnExam {
+
+    private int flipCharacter(final String answerKey, final int k, final char charToFlip) {
+        int left = 0;
+        int count = 0;
+        int length = Integer.MIN_VALUE;
+        for (int right = 0; right < answerKey.length(); ++right) {
+            count += answerKey.charAt(right) == charToFlip ? 1 : 0;
+            while (count > k) {
+                count -= answerKey.charAt(left++) == charToFlip ? 1 : 0;
+            }
+            length = Math.max(length, right - left + 1);
+        }
+        return length;
+    }
+
+    private int maxConsecutiveAnswers(String answerKey, int k) {
+        return Math.max(flipCharacter(answerKey, k, 'T'), flipCharacter(answerKey, k, 'F'));
+    }
+
+    public static void main(String[] args) {
+        String answerKey = "TTFF";
+        int k = 2;
+        System.out.println(new MaximizeTheConfusionOfAnExam().maxConsecutiveAnswers(answerKey, k));
+    }
+}
