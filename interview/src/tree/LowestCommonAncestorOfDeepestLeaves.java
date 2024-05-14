25bbedbd94ee05c5da5d71e5f24b12929f128cec
Akhil-Pathivada <akhil.codec@gmail.com>
2024-05-14 08:13:21 +0530

    learnt@ Tue May 14 08:13:21 IST 2024

---
+++ interview/src/tree/LowestCommonAncestorOfDeepestLeaves.java
@@ -0,0 +1,48 @@
+/**
+ * author: akhilpathivada
+ * time: 14/05/24 07:58
+ *
+ * https://leetcode.com/problems/lowest-common-ancestor-of-deepest-leaves/
+ * https://leetcode.com/problems/smallest-subtree-with-all-the-deepest-nodes/description/
+ *
+ */
+package tree;
+
+public class LowestCommonAncestorOfDeepestLeaves {
+
+    private static final class Pair {
+
+        private final TreeNode node;
+
+        private final int depth;
+
+        private Pair(TreeNode node, int depth) {
+            this.node = node;
+            this.depth = depth;
+        }
+    }
+
+    private Pair lcaDeepestLeaves(TreeNode root, int depth) {
+        if (root == null) {
+            return new Pair(null, depth);
+        }
+        Pair left = lcaDeepestLeaves(root.left, depth + 1);
+        Pair right = lcaDeepestLeaves(root.right, depth + 1);
+        if (left.depth == right.depth) {
+            return new Pair(root, left.depth);
+        }
+        return left.depth > right.depth ? left : right;
+    }
+
+    private TreeNode lcaDeepestLeaves(TreeNode root) {
+        return lcaDeepestLeaves(root, 0).node;
+    }
+
+    public static void main(String[] args) {
+        TreeNode root = new TreeNode(3);
+        root.left = new TreeNode(5);
+        root.left.left = new TreeNode(1);
+        root.right = new TreeNode(1);
+        System.out.println(new LowestCommonAncestorOfDeepestLeaves().lcaDeepestLeaves(root));
+    }
+}
