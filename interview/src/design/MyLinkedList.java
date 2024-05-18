f62f4848b9e2a853f1617fd320a40bdc008c8c6f
Akhil-Pathivada <akhil.codec@gmail.com>
2024-05-18 11:09:04 +0530

    learnt@ Sat May 18 11:09:04 IST 2024

---
+++ interview/src/design/MyLinkedList.java
@@ -0,0 +1,115 @@
+/**
+ * author: akhilpathivada
+ * time: 18/05/24 09:05
+ */
+package design;
+
+public class MyLinkedList {
+
+    private static final class LinkedListNode {
+
+        private final int val;
+
+        private LinkedListNode next;
+
+        private LinkedListNode(int val, LinkedListNode next) {
+            this.val = val;
+            this.next = next;
+        }
+    }
+
+    private LinkedListNode head;
+
+    private LinkedListNode tail;
+
+    public MyLinkedList() {
+        this.head = this.tail = null;
+    }
+
+    public int get(int index) {
+        if (head == null) {
+            return -1;
+        }
+        LinkedListNode current = head;
+        while (index-- > 0) {
+            current = current.next;
+            if (current == null) {
+                return -1;
+            }
+        }
+        return current.val;
+    }
+
+    public void addAtHead(int val) {
+        head = new LinkedListNode(val, head);
+        if (tail == null) {
+            tail = head;
+        }
+    }
+
+    public void addAtTail(int val) {
+        LinkedListNode node = new LinkedListNode(val, null);
+        if (tail == null) {
+            head = node;
+        } else {
+            tail.next = node;
+        }
+        tail = node;
+    }
+
+    public void addAtIndex(int index, int val) {
+        if (index == 0) {
+            addAtHead(val);
+            return;
+        }
+        if (head == null) {
+            return;
+        }
+        int i = 0;
+        LinkedListNode current = head;
+        while (i < index - 1 && current.next != null) {
+            ++i;
+            current = current.next;
+        }
+        if (i < index - 1) {
+            return;
+        }
+        if (current.next == null) { // case: tail
+            addAtTail(val);
+            return;
+        }
+        LinkedListNode node = new LinkedListNode(val, null);
+        node.next = current.next;
+        current.next = node;
+    }
+
+    public void deleteAtIndex(int index) {
+        if (head == null) {
+            return;
+        }
+        if (index == 0) {
+            head = head.next;
+            return;
+        }
+        int i = 0;
+        LinkedListNode current = head;
+        while (i < index - 1 && current.next != null) {
+            ++i;
+            current = current.next;
+        }
+        if (current.next == null) {
+            return;
+        }
+        current.next = current.next.next;
+    }
+
+    public static void main(String[] args) {
+        MyLinkedList myLinkedList = new MyLinkedList();
+        myLinkedList.addAtHead(1);
+        myLinkedList.addAtTail(3);
+        myLinkedList.addAtIndex(1, 2);    // linked list becomes 1->2->3
+        System.out.println(myLinkedList.get(1));              // return 2
+        myLinkedList.deleteAtIndex(1);    // now the linked list is 1->3
+        System.out.println(myLinkedList.get(1));              // return 3
+    }
+}
---
+++ interview/src/linkedlist/MergeInBetweenLinkedLists.java
@@ -0,0 +1,46 @@
+/**
+ * author: akhilpathivada
+ * time: 18/05/24 09:47
+ */
+package linkedlist;
+
+public class MergeInBetweenLinkedLists {
+
+    private LinkedListNode getNthNode(LinkedListNode current, int n) {
+        while (n-- > 0) {
+            current = current.next;
+        }
+        return current;
+    }
+
+    private LinkedListNode getTailOfList(LinkedListNode current) {
+        while (current.next != null) {
+            current = current.next;
+        }
+        return current;
+    }
+
+    public LinkedListNode mergeInBetween(LinkedListNode list1, int a, int b, LinkedListNode list2) {
+        LinkedListNode previousNodeOfA = getNthNode(list1, a - 1);
+        LinkedListNode nextNodeOfB = getNthNode(list1, b + 1);
+        previousNodeOfA.next = list2;
+        getTailOfList(list2).next = nextNodeOfB;
+        return list1;
+    }
+
+    public static void main(String[] args) {
+        LinkedListNode list1 = new LinkedListNode(10);
+        list1.next = new LinkedListNode(1);
+        list1.next.next = new LinkedListNode(13);
+        list1.next.next.next = new LinkedListNode(6);
+        list1.next.next.next.next = new LinkedListNode(9);
+        list1.next.next.next.next.next = new LinkedListNode(5);
+        LinkedListNode list2 = new LinkedListNode(1001);
+        list2.next = new LinkedListNode(1002);
+        list2.next.next = new LinkedListNode(1003);
+        int a = 3;
+        int b = 4;
+        list1 = new MergeInBetweenLinkedLists().mergeInBetween(list1, a, b, list2);
+        list1.printLinkedList(list1);
+    }
+}
