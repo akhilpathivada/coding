a95badec650b5c327b6a660eef7e4ec715acff70
Akhil-Pathivada <akhil.codec@gmail.com>
2024-07-02 10:00:56 +0530

    refactor@ Tue Jul  2 10:00:56 IST 2024

--- interview/src/array/IntersectionOfTwoArraysI.java
+++ interview/src/array/IntersectionOfTwoArraysI.java
@@ -1,10 +1,12 @@
 /**
+ * author: akhilpathivada
+ * time: 02/07/24 09:59
  *
  * https://leetcode.com/problems/intersection-of-two-arrays/
  *
  * Time Complexity : O(N)
  * Space Complexity : O(N)
- * */
+ */
 package array;
 
 import java.util.Arrays;
@@ -12,30 +14,23 @@ import java.util.HashSet;
 import java.util.Set;
 
 public class IntersectionOfTwoArraysI {
-        private static int[] intersectionOfArrays(int[] nums1, int[] nums2) {
-                Set<Integer> set = new HashSet<>();
-                Set<Integer> intersect = new HashSet<>();
-                // insert all elements into set
-                for (int i : nums1) {
-                        set.add(i);
-                }
-                for (int i : nums2) {
-                        // intersection element found: insert into intersection set
-                        if (set.contains(i)) {
-                                intersect.add(i);
-                        }
-                }
-                int[] result = new int[intersect.size()];
-                int i = 0;
-                // add to result array
-                for (int num : intersect) {
-                        result[i++] = num;
-                }
-                return result;
+
+    private static int[] intersectionOfArrays(int[] nums1, int[] nums2) {
+        final Set<Integer> set = new HashSet<>();
+        final Set<Integer> intersect = new HashSet<>();
+        // insert all elements into set
+        Arrays.stream(nums1).forEach(set::add);
+        for (int num : nums2) {
+            if (set.contains(num)) { // intersection element found
+                intersect.add(num);
+            }
         }
-        public static void main(String[] args) {
-                int nums1[] = { 4, 9, 5};
-                int nums2[] = { 9, 4, 9, 8, 4};
-                System.out.println(Arrays.toString(intersectionOfArrays(nums1, nums2)));
-        }
+        return intersect.stream().mapToInt(x -> x).toArray();
+    }
+
+    public static void main(String[] args) {
+        int[] nums1 = {4, 9, 5};
+        int[] nums2 = {9, 4, 9, 8, 4};
+        System.out.println(Arrays.toString(intersectionOfArrays(nums1, nums2)));
+    }
 }
