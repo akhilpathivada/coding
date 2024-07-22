d11ad8f705ad56029beb8050a31728ffed15aa70
akhil.p <akhil.p@HMDVAKHILP.local>
2024-07-22 15:08:10 +0530

    refactor@ Mon Jul 22 15:08:10 IST 2024

--- interview/src/graph/CloneGraph.java
+++ interview/src/graph/CloneGraph.java
@@ -1,50 +1,62 @@
 /**
+ *
  * https://leetcode.com/problems/clone-graph/
+ *
  * */
 
 package graph;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
-public class CloneGraph extends GraphNode {
-        // tracks whether node is already cloned
-        private Map<Integer, GraphNode> map = new HashMap<>();
-        private GraphNode cloneGraph(GraphNode node) {
+public class CloneGraph {
+
+        private final Map<Integer, Node> map = new HashMap<>(); // tracks the cloned nodes
+
+        private static final class Node {
+                
+                private int val;
+
+                private List<Node> neighbors;
+
+                public Node(int _val) {
+                        val = _val;
+                        neighbors = new ArrayList<Node>();
+                }
+        }
+
+        private Node cloneGraph(Node node) {
                 if (node == null) {
                         return null;
                 }
-                // if the node is already formed in Clone graph
-                if (map.containsKey(node.label)) {
-                        return map.get(node.label);
+                if (map.containsKey(node.val)) { // node is already formed in clone graph
+                        return map.get(node.val);
                 }
-                // create new node into Clone graph
-                GraphNode clone = new GraphNode(node.label);
-                // add into map
-                map.put(clone.label, clone);
-                // iterate over all its neighbours
-                for (GraphNode neighbour : node.neighbours) {
-                        clone.neighbours.add(cloneGraph(neighbour));
-                }
+                Node clone = new Node(node.val); // create new node into Clone graph
+                map.put(clone.val, clone); // add into map
+                node.neighbors.forEach(neighbor -> clone.neighbors.add(cloneGraph(neighbor)));
                 return clone;
         }
+        
         public static void main(String[] args) {
                 // create graph with 4 nodes
-                GraphNode[] graph = new GraphNode[4];
-                graph[0] = new GraphNode(1);
-                graph[1] = new GraphNode(2);
-                graph[2] = new GraphNode(3);
-                graph[3] = new GraphNode(4);
-                // adding neighbours to nodes
-                graph[0].neighbours.add(graph[1]);
-                graph[0].neighbours.add(graph[2]);
-                graph[1].neighbours.add(graph[0]);
-                graph[1].neighbours.add(graph[3]);
-                graph[2].neighbours.add(graph[0]);
-                graph[2].neighbours.add(graph[3]);
-                graph[3].neighbours.add(graph[1]);
-                graph[3].neighbours.add(graph[2]);
-                System.out.print("Original Graph : "); new GraphNode().DFS(graph[0]);
-                System.out.print("Cloned Graph : "); new GraphNode().DFS(new CloneGraph().cloneGraph(graph[0]));
+                Node[] graph = new Node[4];
+                graph[0] = new Node(1);
+                graph[1] = new Node(2);
+                graph[2] = new Node(3);
+                graph[3] = new Node(4);
+                // adding neighbors to nodes
+                graph[0].neighbors.add(graph[1]);
+                graph[0].neighbors.add(graph[2]);
+                graph[1].neighbors.add(graph[0]);
+                graph[1].neighbors.add(graph[3]);
+                graph[2].neighbors.add(graph[0]);
+                graph[2].neighbors.add(graph[3]);
+                graph[3].neighbors.add(graph[1]);
+                graph[3].neighbors.add(graph[2]);
+//                System.out.print("Original Graph : "); new Node().DFS(graph[0]);
+//                System.out.print("Cloned Graph : "); new Node().DFS(new CloneGraph().cloneGraph(graph[0]));
         }
 }
