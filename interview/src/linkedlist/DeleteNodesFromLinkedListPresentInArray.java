0886f21e747855cf7b8c68a57760cb5d5e49d557
akhil.p <akhil.p@HMDVAKHILP.local>
2024-08-05 15:20:00 +0530

    learnt@ Mon Aug  5 15:20:00 IST 2024

---
+++ interview/src/linkedlist/DeleteNodesFromLinkedListPresentInArray.java
@@ -0,0 +1,7 @@
+/**
+ * date 05/08/24 14:59
+ * @author akhil.p
+ * 
+ * */
+package linkedlist;public class DeleteNodesFromLinkedListPresentInArray {
+}
---
+++ interview/src/linkedlist/ListNode.java
@@ -0,0 +1,7 @@
+/**
+ * date 05/08/24 15:15
+ * @author akhil.p
+ * 
+ * */
+package linkedlist;public class ListNode {
+}
