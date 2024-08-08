9c498b16a5b3804b43756883390b1a05d32e793e
akhil.p <akhil.p@HMDVAKHILP.local>
2024-08-08 16:08:46 +0530

    learnt@ Thu Aug  8 16:08:29 IST 2024

---
+++ interview/src/slidingwindow/NumberOfSubarraysWithBoundedMaximum.java
@@ -0,0 +1,34 @@
+/**
+ * date 08/08/24 16:00
+ *
+ * @author akhil.p
+ *
+ * https://leetcode.com/problems/number-of-subarrays-with-bounded-maximum/description/
+ *
+ */
+package slidingwindow;
+
+public class NumberOfSubarraysWithBoundedMaximum {
+
+    // https://leetcode.com/problems/number-of-subarrays-with-bounded-maximum/solutions/1278743/c-java-python-easy-to-understand-solution-clean-concise-o-n/
+    private int countOfSubarraysLessThanBound(int[] nums, int bound) {
+        int count = 0;
+        int result = 0;
+        for (int num : nums) {
+            count = num <= bound ? count + 1 : 0;
+            result += count;
+        }
+        return result;
+    }
+
+    private int numSubarrayBoundedMax(int[] nums, int left, int right) {
+        return countOfSubarraysLessThanBound(nums, right) - countOfSubarraysLessThanBound(nums, left - 1);
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {2, 9, 2, 5, 6};
+        int left = 2;
+        int right = 8;
+        System.out.println(new NumberOfSubarraysWithBoundedMaximum().numSubarrayBoundedMax(nums, left, right));
+    }
+}
