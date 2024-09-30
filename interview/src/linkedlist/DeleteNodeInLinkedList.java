56336419234b2a97219c74ca10ac46a3ad8f95d2
Akhil-Pathivada <akhil.codec@gmail.com>
2024-09-30 09:43:14 +0530

    refactor@ Mon Sep 30 09:43:14 IST 2024

--- interview/src/linkedlist/DeleteNodeInLinkedList.java
+++ interview/src/linkedlist/DeleteNodeInLinkedList.java
@@ -8,20 +8,30 @@ package linkedlist;
 
 public class DeleteNodeInLinkedList {
 
-    private void deleteNode(LinkedListNode node) {
-//        if (node == null) {
-//            return;
-//        }
-//        if (node)
+    // approach-1
+    private void deleteNode(ListNode node) {
+        node.val = node.next.val;
+        node.next = node.next.next;
+    }
+
+    // approach-2
+    private void deleteNode_2(ListNode node) {
+        ListNode temp = new ListNode();
+        while (node.next != null) {
+            node.val = node.next.val;
+            temp = node;
+            node = node.next;
+        }
+        temp.next = null;
     }
 
     public static void main(String[] args) {
-        LinkedListNode head = new LinkedListNode(3);
-        head.next = new LinkedListNode(2);
-        head.next.next = new LinkedListNode(4);
-        head.next.next.next = new LinkedListNode(2);
-        head.next.next.next.next = new LinkedListNode(7);
-        head.next.next.next.next.next = new LinkedListNode(5);
+        ListNode head = new ListNode(3);
+        head.next = new ListNode(2);
+        head.next.next = new ListNode(4);
+        head.next.next.next = new ListNode(2);
+        head.next.next.next.next = new ListNode(7);
+        head.next.next.next.next.next = new ListNode(5);
         new DeleteNodeInLinkedList().deleteNode(head);
     }
 }
