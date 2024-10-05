5bff4b13915bc0df8f334456aeb20fb81377d67d
Akhil-Pathivada <akhil.codec@gmail.com>
2024-10-05 08:28:10 +0530

    refactor@ Sat Oct  5 08:28:10 IST 2024

--- interview/src/linkedlist/ReverseLinkedListBetweenRange.java
+++ interview/src/linkedlist/ReverseLinkedListBetweenRange.java
@@ -9,46 +9,35 @@ package linkedlist;
 
 public class ReverseLinkedListBetweenRange {
 
-    private LinkedListNode reverseListForSize(LinkedListNode head, int size) {
-
-        LinkedListNode curr = head;
-        LinkedListNode prev = null;
-        LinkedListNode nextPtr = null;
-        // traverse the elements for the size of the range
-        for (int count = 1; curr != null && count <= size; ++count) {
-            nextPtr = curr.next;
+    // Reverse a sublist of size `length` starting from the head of the list
+    private ListNode reverseSublist(ListNode startNode, int length) {
+        ListNode curr = startNode;
+        ListNode next = null;
+        ListNode prev = null;
+        for (int i = 0; curr != null && i < length; ++i) { // Reverse the list for `length` nodes
+            next = curr.next;
             curr.next = prev;
             prev = curr;
-            curr = nextPtr;
+            curr = next;
         }
-        // point the head of the reversed list to the next of ending node (in original list)
-        head.next = nextPtr;
-        return prev;
+        startNode.next = next; // Connect the last node of the reversed sublist to the remainder of the list
+        return prev; // Return the new head of the reversed sublist
     }
 
-    private LinkedListNode reverseBetween(LinkedListNode head, int left, int right) {
-        LinkedListNode dummy = new LinkedListNode();
-        dummy.next = head;
-        // starting node from where the reversal should start
-        LinkedListNode startingNode = dummy;
-        // traverse till the node before the starting node
-        for (int i = 1; i < left; ++i) {
-            startingNode = startingNode.next;
+    private ListNode reverseBetween(ListNode head, int left, int right) {
+        final ListNode dummy = new ListNode(0, head);
+        ListNode nodeBeforeLeft = dummy; // Node before the `left` node
+        for (int i = 1; i < left; ++i) { // Traverse to the node just before the `left` position
+            nodeBeforeLeft = nodeBeforeLeft.next;
         }
-        // point the node before the starting node to head of reversed list
-        startingNode.next = reverseListForSize(startingNode.next, right - left + 1);
-        // always new head would be next of the dummy
+        nodeBeforeLeft.next = reverseSublist(nodeBeforeLeft.next, right - left + 1); // Reverse the sublist from `left` to `right`
         return dummy.next;
     }
 
     public static void main(String[] args) {
-        LinkedListNode head = new LinkedListNode(1);
-        head.next = new LinkedListNode(2);
-        head.next.next = new LinkedListNode(3);
-        head.next.next.next = new LinkedListNode(4);
-        head.next.next.next.next = new LinkedListNode(5);
-        head.next.next.next.next.next = new LinkedListNode(6);
+        int[] nums = {1, 2, 3, 4, 5, 6};
+        ListNode head = ListNode.createLinkedListFromArray(nums);
         int left = 2, right = 4;
-        head.printLinkedList(new ReverseLinkedListBetweenRange().reverseBetween(head, left, right));
+        ListNode.printLinkedList(new ReverseLinkedListBetweenRange().reverseBetween(head, left, right));
     }
 }
