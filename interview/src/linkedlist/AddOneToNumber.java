6551d61f46534cf23ddd64db93f77c12f14df252
Akhil-Pathivada <akhil.codec@gmail.com>
2024-10-06 17:37:28 +0530

    refactor@ Sun Oct  6 17:37:27 IST 2024

--- interview/src/linkedlist/AddOneToNumber.java
+++ interview/src/linkedlist/AddOneToNumber.java
@@ -5,47 +5,48 @@
  *
  * https://www.geeksforgeeks.org/add-1-number-represented-linked-list/
  *
- * Time Complexity : O(N)
+ * Time Complexity : O(n)
  * Space Complexity : O(1)
  */
 package linkedlist;
 
 public class AddOneToNumber {
-        private LinkedListNode reverse(LinkedListNode head) {
-                LinkedListNode curr = head, prev = null, next;
-                while (curr != null) {
-                        next = curr.next;
-                        curr.next = prev;
-                        prev = curr;
-                        curr = next;
-                }
-                return prev;
+
+    private ListNode reverse(ListNode head) {
+        ListNode current = head, prev = null, next;
+        while (current != null) {
+            next = current.next;
+            current.next = prev;
+            prev = current;
+            current = next;
         }
-        private LinkedListNode addOne(LinkedListNode head) {
-                head = reverse(head);
-                LinkedListNode curr = head, prev = null;
-                int carry = 1;
-                while (curr != null) {
-                        int sum = carry + curr.data;
-                        carry = sum / 10;
-                        curr.data = sum % 10;
-                        prev = curr;
-                        curr = curr.next;
-                        if (carry == 0) {
-                                break;
-                        }
-                }
-                if (carry == 1) {
-                        prev.next = new LinkedListNode(1);
-                }
-                return reverse(head);
+        return prev;
+    }
+
+    private ListNode addOne(ListNode head) {
+        ListNode reversedHead = reverse(head);
+        ListNode current = reversedHead;
+        ListNode prev = null;
+        int carry = 1;
+        while (current != null) {
+            int sum = carry + current.val;
+            carry = sum / 10;
+            current.val = sum % 10;
+            prev = current;
+            current = current.next;
+            if (carry == 0) {
+                break;
+            }
         }
-        public static void main(String[] args) {
-                LinkedListNode head = new LinkedListNode(9);
-                head.next = new LinkedListNode(9);
-                head.next.next = new LinkedListNode(9);
-                head.next.next.next = new LinkedListNode(9);
-                head = new AddOneToNumber().addOne(head);
-                head.printLinkedList(head);
+        if (carry == 1) {
+            prev.next = new ListNode(1);
         }
+        return reverse(reversedHead);
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {9, 9, 9, 9};
+        ListNode head = LinkedListUtil.buildLinkedListFromArray(nums);
+        LinkedListUtil.printLinkedList(new AddOneToNumber().addOne(head));
+    }
 }
