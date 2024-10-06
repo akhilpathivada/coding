6a3d808ef82743d6354a6d9266c5b68c0e32111a
Akhil-Pathivada <akhil.codec@gmail.com>
2024-10-06 17:53:25 +0530

    refactor@ Sun Oct  6 17:53:25 IST 2024

--- interview/src/linkedlist/DeleteNodeInLinkedList.java
+++ interview/src/linkedlist/DeleteNodeInLinkedList.java
@@ -26,12 +26,9 @@ public class DeleteNodeInLinkedList {
     }
 
     public static void main(String[] args) {
-        ListNode head = new ListNode(3);
-        head.next = new ListNode(2);
-        head.next.next = new ListNode(4);
-        head.next.next.next = new ListNode(2);
-        head.next.next.next.next = new ListNode(7);
-        head.next.next.next.next.next = new ListNode(5);
+        int[] nums = {3, 2, 4, 2, 7, 5};
+        ListNode head = LinkedListUtil.buildLinkedListFromArray(nums);
         new DeleteNodeInLinkedList().deleteNode(head);
+        LinkedListUtil.printLinkedList(head);
     }
 }
--- interview/src/linkedlist/DeleteNodesFromLinkedListPresentInArray.java
+++ interview/src/linkedlist/DeleteNodesFromLinkedListPresentInArray.java
@@ -17,28 +17,19 @@ public class DeleteNodesFromLinkedListPresentInArray {
     private ListNode modifiedList(int[] nums, ListNode head) {
         final Set<Integer> set = Arrays.stream(nums).boxed().collect(Collectors.toSet());
         final ListNode dummy = new ListNode(0, head);
-        ListNode current = head;
-        ListNode prev = dummy;
-        while (current != null) {
-            if (set.contains(current.val)) {
-                prev.next = current.next;
+        for (ListNode current = dummy; current.next != null; ) {
+            if (set.contains(current.next.val)) {
+                current.next = current.next.next;
             } else {
-                prev = current;
+                current = current.next;
             }
-            current = current.next;
         }
         return dummy.next;
     }
 
     public static void main(String[] args) {
-        ListNode head = new ListNode(1);
-        head.next = new ListNode(2);
-        head.next.next = new ListNode(3);
-        head.next.next.next = new ListNode(4);
-        head.next.next.next.next = new ListNode(5);
-        head.next.next.next.next.next = new ListNode(6);
+        ListNode head = LinkedListUtil.buildLinkedListFromArray(new int[]{1, 2, 3, 4, 5});
         int[] nums = {1, 2, 3};
-        head = new DeleteNodesFromLinkedListPresentInArray().modifiedList(nums, head);
-        // head.printLinkedList(head);
+        LinkedListUtil.printLinkedList(new DeleteNodesFromLinkedListPresentInArray().modifiedList(nums, head));
     }
 }
