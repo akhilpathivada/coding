bc4a3522d30a656f92f333b9d3e245129dc35b1b
Akhil-Pathivada <akhil.codec@gmail.com>
2024-10-06 18:05:25 +0530

    refactor@ Sun Oct  6 18:05:25 IST 2024

--- interview/src/linkedlist/AddTwoNumbers.java
+++ interview/src/linkedlist/AddTwoNumbers.java
@@ -5,49 +5,37 @@
  *
  * https://leetcode.com/problems/add-two-numbers/
  *
- * Time Complexity : O(N)
+ * Time Complexity : O(n)
  * Space Complexity : O(1)
  */
 package linkedlist;
 
-public class AddTwoNumbers extends LinkedListNode {
-        private LinkedListNode addNumbers(LinkedListNode l1, LinkedListNode l2) {
-                LinkedListNode dummy = new LinkedListNode(0); // creating an dummy list
-                LinkedListNode current = dummy;
-                int carry = 0; // intialising our carry with 0 intiall
-                // while loop will run, until l1 OR l2 not reaches null OR if they both reaches null. But our carry has some value in it.
-                // We will add that as well into our list
-                while (l1 != null || l2 != null || carry == 1) {
-                        int sum = 0;
-                        // adding l1 to our sum & moving l1
-                        if (l1 != null) {
-                                sum += l1.data;
-                                l1 = l1.next;
-                        }
-                        // adding l2 to our sum & moving l2
-                        if (l2 != null) {
-                                sum += l2.data;
-                                l2 = l2.next;
-                        }
-                        sum += carry; // if we have carry then add it into our sum
-                        carry = sum / 10; // if we get carry, then divide it by 10 to get the carry
-                        current.next = new LinkedListNode(sum % 10);  // the value we'll get by moduloing it, will become as new node so. add it to our list
-                        current = current.next; // update the current every time
-                }
-                return dummy.next;
+public class AddTwoNumbers {
+
+    private ListNode addNumbers(ListNode l1, ListNode l2) {
+        final ListNode dummyHead = new ListNode(0);
+        ListNode current = dummyHead;
+        int carry = 0; // Initialize carry to 0
+        while (l1 != null || l2 != null || carry == 1) { // Continue the loop while there are nodes in l1, l2, or there is a carry
+            int sum = carry; // Start with the carry from the previous iteration
+            if (l1 != null) { // Add the value from l1 if it's not null
+                sum += l1.val;
+                l1 = l1.next;
+            }
+            if (l2 != null) { // Add the value from l2 if it's not null
+                sum += l2.val;
+                l2 = l2.next;
+            }
+            carry = sum / 10; // Update carry for the next iteration
+            current.next = new ListNode(sum % 10);  // Add the digit to the result list
+            current = current.next; // Move the pointer to the next node in the result list
         }
-        public static void main(String[] args) {
-                LinkedListNode list1 = new LinkedListNode(9);
-                list1.next = new LinkedListNode(9);
-                list1.next.next = new LinkedListNode(9);
-                list1.next.next.next = new LinkedListNode(9);
-                list1.next.next.next.next = new LinkedListNode(9);
-                list1.next.next.next.next.next = new LinkedListNode(9);
-                list1.next.next.next.next.next.next  = new LinkedListNode(9);
-                LinkedListNode list2 = new LinkedListNode(9);
-                list2.next = new LinkedListNode(9);
-                list2.next.next = new LinkedListNode(9);
-                list2.next.next.next = new LinkedListNode(9);
-                new LinkedListNode().printLinkedList(new AddTwoNumbers().addNumbers(list1, list2));
-        }
+        return dummyHead.next; // Return the next of dummyHead, as it's the actual head of the result
+    }
+
+    public static void main(String[] args) {
+        ListNode list1 = LinkedListUtil.buildLinkedListFromArray(new int[]{9, 9, 9, 9, 9, 9, 9});
+        ListNode list2 = LinkedListUtil.buildLinkedListFromArray(new int[]{9, 9, 9, 9});
+        LinkedListUtil.printLinkedList(new AddTwoNumbers().addNumbers(list1, list2));
+    }
 }
