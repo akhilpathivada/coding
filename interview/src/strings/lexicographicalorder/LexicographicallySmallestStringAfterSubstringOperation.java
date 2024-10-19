2e009c80c04a9732b33da7338b8e7f1186b6958f
Akhil-Pathivada <akhil.codec@gmail.com>
2024-10-19 08:44:22 +0530

    refactor@ Sat Oct 19 08:44:22 IST 2024

--- interview/src/strings/lexicographicalorder/LexicographicallySmallestStringAfterSubstringOperation.java
+++ interview/src/strings/lexicographicalorder/LexicographicallySmallestStringAfterSubstringOperation.java
@@ -9,10 +9,7 @@ package strings.lexicographicalorder;
 
 public class LexicographicallySmallestStringAfterSubstringOperation {
 
-    private boolean containsOnlyA(final String s) {
-        return s.chars().allMatch(ch -> ch == 'a');
-    }
-
+    // Decrements non-'a' characters starting from the given index
     private String decrementCharacters(final char[] chars, final int start) {
         int n = chars.length;
         for (int i = start; i < n && chars[i] != 'a'; ++i) {
@@ -21,18 +18,29 @@ public class LexicographicallySmallestStringAfterSubstringOperation {
         return new String(chars);
     }
 
-    private String smallestString(String s) {
-        char[] chars = s.toCharArray();
-        int n = s.length();
-        if (containsOnlyA(s)) {
-            chars[n - 1] = 'z'; // If the string contains only 'a', change the last character to 'z'
-            return new String(chars);
-        }
+    // Find the index of first non-'a' character
+    private int findIndexOfFirstNonA(final char[] chars, final int n) {
         int indexOfFirstNonA = 0;
         while (indexOfFirstNonA < n && chars[indexOfFirstNonA] == 'a') { // Find the first non-'a' character
             ++indexOfFirstNonA;
         }
-        return decrementCharacters(chars, indexOfFirstNonA);
+        return indexOfFirstNonA;
+    }
+
+    // Checks if the string contains only 'a's
+    private boolean containsOnlyA(final String s) {
+        return s.chars().allMatch(ch -> ch == 'a');
+    }
+
+    private String smallestString(String s) {
+        char[] chars = s.toCharArray();
+        int n = s.length();
+        if (containsOnlyA(s)) {
+            chars[n - 1] = 'z';  // If only 'a's, change the last character to 'z'
+            return new String(chars);
+        }
+        int indexOfFirstNonA = findIndexOfFirstNonA(chars, n);
+        return decrementCharacters(chars, indexOfFirstNonA); // Decrement characters after the first non-'a' character
     }
 
     public static void main(String[] args) {
