e9773f6453f6bb97d5000cd2aec6cc47416b6fa2
Akhil-Pathivada <akhil.codec@gmail.com>
2024-10-19 13:59:13 +0530

    refactor@ Sat Oct 19 13:59:13 IST 2024

--- interview/src/heap/LeastNumberOfUniqueIntegersAfterKRemovals.java
+++ interview/src/heap/LeastNumberOfUniqueIntegersAfterKRemovals.java
@@ -11,16 +11,25 @@ import java.util.*;
 
 public class LeastNumberOfUniqueIntegersAfterKRemovals {
 
-    private int findLeastNumOfUniqueInts(int[] arr, int k) {
-        final Map<Integer, Integer> map = new HashMap<>();
-        for (int num : arr) {
-            map.put(num, map.getOrDefault(num, 0) + 1);
-        }
-        final PriorityQueue<Integer> maxHeap = new PriorityQueue<>(map.values());
+    public int findLeastNumOfUniqueInts(int[] arr, int k) {
+        Map<Integer, Integer> frequencyMap = buildFrequencyMap(arr);
+        PriorityQueue<Integer> frequencyQueue = buildFrequencyQueue(frequencyMap);
         while (k > 0) {
-            k -= maxHeap.poll();
+            k -= frequencyQueue.poll();
         }
-        return k < 0 ? maxHeap.size() + 1 : maxHeap.size();
+        return k < 0 ? frequencyQueue.size() + 1 : frequencyQueue.size();
+    }
+
+    // Build a frequency map to count occurrences of each integer
+    private Map<Integer, Integer> buildFrequencyMap(final int[] arr) {
+        Map<Integer, Integer> frequencyMap = new HashMap<>();
+        Arrays.stream(arr).forEach(num -> frequencyMap.put(num, frequencyMap.getOrDefault(num, 0) + 1));
+        return frequencyMap;
+    }
+
+    // Build a priority queue (min-heap) by adding all the frequencies
+    private PriorityQueue<Integer> buildFrequencyQueue(final Map<Integer, Integer> frequencyMap) {
+        return new PriorityQueue<>(frequencyMap.values()); // Add all frequencies to a min-heap
     }
 
     public static void main(String[] args) {
